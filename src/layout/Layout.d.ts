a6d66e750aeab692fb87297158c20edb9cd9d334
baptaste <babachampbenoit@gmail.com>
2022-07-26 21:35:45 +0200

    adding layout back (renamed and untracked)

---
+++ src/layout/Layout.d.ts
@@ -0,0 +1,5 @@
+import type { ReactNode } from "react";
+
+export declare interface ILayout {
+    children?: ReactNode
+}
---
+++ src/layout/index.tsx
@@ -0,0 +1,17 @@
+import { PropsWithChildren, useState } from 'react';
+import Auth from '../store/containers/Auth';
+import TabBar from '../store/containers/TabBar';
+import { logAny } from '../utils/logs.utils';
+import styles from './index.module.scss';
+import { ILayout } from './Layout';
+
+export default function Layout({ children }: PropsWithChildren<ILayout>): JSX.Element {
+	return (
+		<div className='app'>
+			<Auth>
+				<main className={`${styles['layout']} flex`}>{children}</main>
+				<TabBar />
+			</Auth>
+		</div>
+	);
+}
